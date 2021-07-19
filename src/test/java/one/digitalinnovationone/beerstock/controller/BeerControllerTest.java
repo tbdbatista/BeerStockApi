a50e806a49a576d56bd845cb29a6ba511ffdaeaa
tbdbatista <tbdbatista@gmail.com>
2021-07-19 14:01:06 -0300

    implemented whenPOSTIsCalledThenABeerIsCreated in BeerControllerTest

--- src/test/java/one/digitalinnovationone/beerstock/controller/BeerControllerTest.java
+++ src/test/java/one/digitalinnovationone/beerstock/controller/BeerControllerTest.java
@@ -1,23 +1,35 @@
 package one.digitalinnovationone.beerstock.controller;
 
+
 import one.digitalinnovation.beerstock.controller.BeerController;
+import one.digitalinnovation.beerstock.dto.BeerDTO;
 import one.digitalinnovation.beerstock.service.BeerService;
+import one.digitalinnovationone.beerstock.builder.BeerDTOBuilder;
+import org.hamcrest.core.Is;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+import static one.digitalinnovationone.beerstock.utils.JsonConvertionUtils.asJsonString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 public class BeerControllerTest {
 
     private static final String BEER_API_URL_PATH = "/api/v1/beers";
     private static final long VALID_BEER_ID = 1L;
-    private static final long INVALID_BEER_ID = 2l;
+    private static final long INVALID_BEER_ID = 2L;
     private static final String BEER_API_SUBPATH_INCREMENT_URL = "/increment";
     private static final String BEER_API_SUBPATH_DECREMENT_URL = "/decrement";
 
@@ -37,6 +49,21 @@ public class BeerControllerTest {
                 .build();
     }
 
+    @Test
+    void whenPOSTIsCalledThenABeerIsCreated() throws Exception {
+
+        BeerDTO beerDTO = BeerDTOBuilder.builder().build().toBeerDTO();
+
+        when(beerService.createBeer(beerDTO)).thenReturn(beerDTO);
+
+        mockMvc.perform(post(BEER_API_URL_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(beerDTO)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name", Is.is(beerDTO.getName())))
+                .andExpect(jsonPath("$.brand", Is.is(beerDTO.getBrand())))
+                .andExpect(jsonPath("$.type", Is.is(beerDTO.getType().toString())));
+    }
 
 
 }
