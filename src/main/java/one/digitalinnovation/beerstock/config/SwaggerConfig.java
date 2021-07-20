6beecc12fb84c419eff46b67e2e38bf440c75848
tbdbatista <tbdbatista@gmail.com>
2021-07-20 16:30:09 -0300

    implemented whenPATCHIsCalledToIncrementDiscountThenOKstatusIsReturned. And Swagger configuration is done.

--- src/main/java/one/digitalinnovation/beerstock/config/SwaggerConfig.java
+++ src/main/java/one/digitalinnovation/beerstock/config/SwaggerConfig.java
@@ -1,4 +1,41 @@
 package one.digitalinnovation.beerstock.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
+
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
+
+    private static final String BASE_PACKAGE = "one.digitalinnovation.beerstock.controller";
+    private static final String TITLE = "Beer Stock API";
+    private static final String DESCRIPTION = "REST API for beer stock management";
+
+    private ApiInfo buildApiInfo() {
+        return new ApiInfoBuilder()
+                .title(TITLE)
+                .description(DESCRIPTION)
+                .version("1.0.0")
+                .contact(new Contact("Thiago Beltrami Dias Batista", "https://github.com/tbdbatista", "tbdbatista@gmail.com"))
+                .build();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(basePackage(BASE_PACKAGE))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(buildApiInfo());
+    }
 }
--- src/test/java/one/digitalinnovationone/beerstock/controller/BeerControllerTest.java
+++ src/test/java/one/digitalinnovationone/beerstock/controller/BeerControllerTest.java
@@ -142,4 +142,24 @@ public class BeerControllerTest {
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound());
     }
+
+    @Test
+    void whenPATCHIsCalledToIncrementDiscountThenOKstatusIsReturned() throws Exception {
+        QuantityDTO quantityDTO = QuantityDTO.builder()
+                .quantity(10)
+                .build();
+
+        BeerDTO beerDTO = BeerDTOBuilder.builder().build().toBeerDTO();
+        beerDTO.setQuantity(beerDTO.getQuantity() + quantityDTO.getQuantity());
+
+        when(beerService.increment(VALID_BEER_ID, quantityDTO.getQuantity())).thenReturn(beerDTO);
+
+        mockMvc.perform(MockMvcRequestBuilders.patch(BEER_API_URL_PATH + "/" + VALID_BEER_ID + BEER_API_SUBPATH_INCREMENT_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(quantityDTO))).andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(beerDTO.getName())))
+                .andExpect(jsonPath("$.brand", is(beerDTO.getBrand())))
+                .andExpect(jsonPath("$.type", is(beerDTO.getType().toString())))
+                .andExpect(jsonPath("$.quantity", is(beerDTO.getQuantity())));
+    }
 }
